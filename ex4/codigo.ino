7d12c1203f50bcdbda728533e3c8627b9f3a8e87
Maria Luiza <80901977+malavagirl@users.noreply.github.com>
2022-03-23 08:37:15 -0300

    Create codigo.ino

---
+++ ex4/codigo.ino
@@ -0,0 +1,35 @@
+int LED = 2; //LED conectado ao pino 13
+int BOTAO = 3; //Botão conectado ao pino 8
+int ESTADO_BOTAO = 0; //Variável para leitura do estado do Botão
+int VAR = 0;
+int VAR2 = 0;
+
+void setup()
+
+{
+ pinMode(LED, OUTPUT); //Pino 10 do arduino como saída
+ pinMode(BOTAO, INPUT); //Pino com botão será entrada
+}
+
+void loop()
+
+{
+ VAR = digitalRead(BOTAO); //Armazena o Estado do botão.
+ //Se sim grava LOW (0) na variável
+ //Se não grava HIGH (1) na variável
+ if (VAR == 1 && VAR2 == 0) //Se botão estiver pressionado (HIGH)
+ {
+ ESTADO_BOTAO = 1 - ESTADO_BOTAO;
+   delay(20);
+ }
+ VAR2=VAR;
+  if(ESTADO_BOTAO == 1){
+    
+ digitalWrite(LED, 1);//Acende o led conectado ao pino 13
+
+ }
+ else //se não estiver pressionado
+ {
+ digitalWrite(LED, 0); //Apaga o led conectado ao pino 13
+ }
+}
